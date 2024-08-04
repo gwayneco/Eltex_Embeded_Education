8797ae5e57d84387ea4091f68e67756e9ab1a932
gwayneco <user@sputn>
2024-08-05 02:07:16 +0700

    Completed Task 6.2 and repair Makefile in 6.1 add lib create commands

---
+++ Module2/Task6.2/lib/list.h
@@ -0,0 +1,31 @@
+#ifndef SRC_LIST_H_
+#define SRC_LIST_H_
+
+#define SIZE_STR 20
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Contact {
+  int primary_key;
+  char firstname[20];
+  char secondname[20];
+  char otchestvo[20];
+  char phone_number[12];
+  char work_place[20];
+  char work_post[20];
+  char media[50];
+} Contact;
+
+typedef struct Item {
+  Contact value;
+  struct Item *next;
+  struct Item *prev;
+} Item;
+
+Item* insert_item(Contact person, Item *head);
+Item *delete_item(int val, struct Item *head);
+Item* delete_list(struct Item* head);
+
+#endif  // SRC_LIST_H_
